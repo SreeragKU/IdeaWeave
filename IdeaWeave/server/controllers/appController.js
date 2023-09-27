20017986e0274278025dbfdf6c3c6f3e93d3dbf3
Sreerag K U <118956614+SreeragKU@users.noreply.github.com>
2023-09-27 07:21:53 +0530

    last update for today

--- IdeaWeave/server/controllers/appController.js
+++ IdeaWeave/server/controllers/appController.js
@@ -3,6 +3,21 @@ import bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
 import ENV from '../config.js';
 
+//middleware for verify user
+export async function verifyUser(req, res, next){
+    try{
+        const { username } = req.method == "GET" ? req.query : req.body;
+
+        //check the user
+        let exist = await UserModel.findOne({ username });
+        if(!exist) return res.status(404).send({ error : "Can't find User!"});
+        next();
+    }catch (error) {
+        return res.status(404).send({error : "Authentication Error"});
+    }
+}
+
+
 export async function register(req, res) {
     try {
         const { username, password, profile, email } = req.body;
@@ -86,7 +101,18 @@ export async function login(req, res) {
 
 
 export async function getUser(req, res){
-    res.json('getUser route');
+    const { username } = req.params;
+    try{
+        if(!username) return res.status(501).send({error: "Invalid Username"});
+        UserModel.findOne({username}, function(err, user){
+            if(err) return res.status(500).send({err});
+            if(!user) return res.status(501).send({error : "Couldn't find the User"});
+
+            return res.status(201).send(user);
+        })
+    }catch (error){
+        return res.status(404).send({error : "Cannot find User data"});
+    }
 }
 
 export async function generateOTP(req, res){
