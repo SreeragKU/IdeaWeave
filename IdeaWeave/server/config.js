91abd63ed25f8d41a3d6d3065ea9fd33abab83e0
Sreerag K U <118956614+SreeragKU@users.noreply.github.com>
2023-10-01 18:54:54 +0530

    fragment 1

--- IdeaWeave/server/config.js
+++ IdeaWeave/server/config.js
@@ -1,5 +1,6 @@
-export default {
-    JWT_SECRET : "E5DQPfsEQOnTKNdEEW4QvU2ckX8lp6R+meJTaMjtgWU=",
-    GMAIL_EMAIL: "ideaweaveproject@gmail.com",
-    GMAIL_PASSWORD: "ofckjiefejgtyrly"
-}
+
+// const DATABASE = "mongodb+srv://dbuser:dbpassword@your_db_name.pbn7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DATABASE = "mongodb://localhost:27017/dbname";
+const JWT_SECRET = "some_secret_letters_numbers";
+const SENDGRID_KEY = "SG.your.secret-key";
+const EMAIL_FROM = "yourname@gmail.com";
